2e8059aa7cacd785d1267b10d760096b3ca2df8e
John Doe <ruben.dajcman@sers.si>
2024-01-12 12:36:29 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './source_files/logo.svg';
+import './App.css';
+function Nekaj(){
+  return(<>
+    <div>NNNNNAJJJJJAICII</div>
+    <p>XXXXXDSSDSDSDSDS</p></>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>LI E 2 MOY FAAJJAJJCJEJEJJCJEJCJEJCEJEJ</p>
+        <Nekaj/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
