eabe26edafa899779eea7035c897fe58823efc48
Your Name <ruben.dajcman@sers.si>
2024-01-26 11:25:32 +0100

    a

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import './App.css';
 import Neke from './fragment1/Neke.jsx';
 import Frag from './fragment12/Frag.jsx';
 import {useState} from 'react';
+import { Navbar } from 'react-bootstrap';
+import { Route,Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Neke></Neke>
-        <Frag></Frag>
-      </header>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Neke/>}/>
+        <Route path="/frag" element={<Frag/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import {useEffect, useState} from 'react';
+import { useRef } from 'react';
+import { NavItem, NavbarText } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+function Navbar(){
+
+ 
+    return(
+        <>
+        <nav>
+            <ul>
+                <li>
+            <NavItem>
+                <NavbarText>
+                    <Link to="/">Home</Link>
+                </NavbarText>
+            </NavItem>
+                </li>
+                <li>
+                    <NavItem>
+                    <NavbarText>
+                        <Link to="frag">Frag</Link>
+                    </NavbarText>
+                    </NavItem>
+                </li>
+            </ul>
+        </nav>
+        </>
+    )
+  }
+  export default Navbar;
